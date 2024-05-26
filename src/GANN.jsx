7c56bb38f1fab911551b4351958228da4169f10e
taurn kale <tkumarkale@gmail.com>
2024-05-26 21:24:25 +0530

    ok

--- src/GANN.jsx
+++ src/GANN.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const GANN = () => {
+  let initialName = [
+    { name: 'taun', class: 'kla' },
+    { name: 'taun', class: 'kla' },
+  ];
 
+  const [first, setFirst] = useState(initialName);
+  console.log(first[0].name);
 
-  let name= [ 
-{name:'taun',class:'kla'},
-{name:'taun',class:'kla'},
-]
-
-const [first,setfirst]=useState(name)
-  console.log(name[0].name)
-
-  const hello=()=>{
-   setfirst((pre)=>[...pre[1],{name:'tarun',class:'7'}])
-  }
+  const hello = () => {
+    setFirst((prev) =>
+      prev.map((item, index) => 
+        index === 1 ? { ...item, name: 'tarun', class: '7' } : item
+      )
+    );
+  };
 
   return (
     <div>
-      <button onClick={hello} className='border-blue-950 bg-gray-500'> click</button>
-      {first.name}
+      <button onClick={hello} className='border-blue-950 bg-gray-500'>Click</button>
+      <div>{first[1].name}</div>
     </div>
-  )
-}
+  );
+};
 
-export default GANN
+export default GANN;
