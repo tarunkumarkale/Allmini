7ad81d03245021af6565e88310be2efae1a4dd90
taurn kale <tkumarkale@gmail.com>
2024-05-29 20:08:09 +0530

    hello

--- src/AllComents.jsx
+++ src/AllComents.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const AllComents = ({}) => {
+const AllComents = ({imgg,data}) => {
   return (
     <div>
       <ul> 
-      
-      <li> </li>
+      <li className='flex flex-row font-bold text-xl'><img src={imgg} className='rounded-full w-10 h-10' alt="" /> {data} </li>
+
       </ul>
     </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,47 @@
-import React, { useState } from 'react'
-import { FaArrowUp } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
 import AllComents from './AllComents';
+
 const App = () => {
+  const [first, setFirst] = useState('');
+  const [Second, setSecond] = useState([]);
+  const [images, setImages] = useState([]);
 
+  let arry = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const [first,setfirst]=useState('')
+  const OK = (e) => {
+    let final = e.target.value;
+    setFirst(final);
+  };
 
+  const Todo = () => {
+    setSecond((prev) => [...prev, first]);
+    setFirst('');
+  };
 
-
-
-
-cosnt[Second,setSecond]=useState([])
-
-
-const OK=()=>{
-  setfirst((pre)=>[...pre,first])
-}
-const Todo=()=>{
-setSecond(first)
-setfirst('')
-}
-
-
-
-
+  useEffect(() => {
+    const imageUrls = arry.map((ele) => `https://api.dicebear.com/7.x/notionists/svg?seed=John${ele}&size=64`);
+    setImages(imageUrls);
+  }, []);
 
   return (
+    <>
+      {Second.map((Ele, index) => (
+        <AllComents data={Ele} key={index} imgg={images[index % images.length]} />
+      ))}
+      <div className="flex justify-center items-center h-screen ">
+        <div className="wrap-input-8 relative">
+          <input className="input" type="text" placeholder="Name" onChange={OK} value={first} />
+          <button className="btn absolute bottom-0 right-0 bg-purple-600 text-white rounded-full w-10 h-10 flex items-center justify-center hover:bg-purple-700" onClick={Todo}>
+            <FaArrowUp />
+          </button>
+          <span className="focus-border">
+            <i></i>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-<>
-{
-  Second.map((Ele,index) => <AllComents data={Ele}  key={index} imgg="" />)
-}
-
-    <div className='flex justify-center items-end h-screen'>
-    <div class="wrap-input-8" >
-  <input class="input" type="text" placeholder="Name"  onChange={OK}/>
-   <button class="btn absolute bottom-0 right-0 bg-purple-600 text-white rounded-full w-10 h-10 flex items-center justify-center hover:bg-purple-700" onClick={Todo}>
-   <FaArrowUp />
-  </button>
-  <span class="focus-border">
-    <i></i>
-  </span>
-</div>
-</div>
-</>
-  )
-}
-
-export default App
+export default App;
