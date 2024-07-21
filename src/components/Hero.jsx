f84abd334a94b1e9b7de8449fcbed8e26214decd
taurn kale <tkumarkale@gmail.com>
2024-07-22 02:03:41 +0530

    hello taurn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,31 @@
+
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import { myphoto } from '../Constant/Index';
 
 const HeroSection = () => {
   return (
     <div className="relative w-full h-screen flex flex-col md:flex-row">
-      <div className="w-full md:w-[70%]  bg-black text-white flex flex-col items-start justify-center md:p-11">
-      <h1 className="text-2xl md:text-4xl mb-4 md:mb-6 font-bold text-green-600">
-  Hi, I'm Tarun Kale
-</h1>
-<p className="text-lg md:text-3xl mb-4 md:mb-6 text-green-500">
-  Frontend Developer
-</p>
-<p className="text-lg md:text-3xl mb-4 md:mb-6 text-green-500">
-  JavaScript and React.js Specialist
-</p>
-<p className="text-lg md:text-3xl mb-4 md:mb-6 text-green-500">
-  UI Designer
-</p>
+      <div className="w-full md:w-[70%] bg-black text-white flex flex-col items-start justify-center md:p-11">
+        <h1 className="text-2xl md:text-5xl mb-4 md:mb-6 font-bold text-green-600">
+          Hi, I'm Tarun Kale
+        </h1>
+        <TypeAnimation
+          sequence={[
+            "Frontend Developer",
+            1000, // Waits 1s
+            "JavaScript and React.js Specialist",
+            1000, // Waits 1s
+            "UI Designer",
+            1000, // Waits 1s
+          ]}
+          wrapper="p"
+          cursor={true}
+          repeat={Infinity}
+          className="text-lg md:text-4xl mb-4 md:mb-6 text-green-500"
+        />
       </div>
-      <div className="w-[100%] sm:w-[30%] sm:bg-green-500  bg-black flex items-center justify-center">
+      <div className="w-[100%] sm:w-[30%] sm:bg-green-500 bg-black flex items-center justify-center">
         {/* Image Container */}
         <div className="absolute inset-y-0 left-[50%] md:left-[69%] transform -translate-x-1/2 flex items-center justify-center">
           <img
